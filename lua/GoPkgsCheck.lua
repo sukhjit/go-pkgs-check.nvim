a2c17b63a90516980e6f9a5d5a70f867eefd135e
Sukhjit Singh <sukhjit.singh@sbs.com.au>
2024-12-29 06:16:06 +1100

    Print modules and versions

--- lua/GoPkgsCheck.lua
+++ lua/GoPkgsCheck.lua
@@ -4,4 +4,31 @@ M.setup = function()
   -- nothing
 end
 
+M.check = function()
+  local query_string = "(require_directive) @require_spec"
+
+  local parser = require("nvim-treesitter.parsers").get_parser()
+
+  local query = vim.treesitter.query.parse(parser:lang(), query_string)
+
+  local tree = parser:parse()[1]
+
+  for _, node in query:iter_captures(tree:root(), 0) do
+    vim.print("")
+
+    for _, req_spec in pairs(node:named_children()) do
+      local mod_path = req_spec:named_child(0)
+      local ver = req_spec:named_child(1)
+
+      if mod_path == nil or ver == nil then
+        return
+      end
+
+      print(vim.treesitter.get_node_text(mod_path, 0), vim.treesitter.get_node_text(ver, 0))
+
+      -- go list -m -u -mod=readonly github.com/aws/aws-sdk-go-v2
+    end
+  end
+end
+
 return M
